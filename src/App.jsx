216b493d27abb424f4374417847882ae1182c6cb
LeeBingler <binglerlee@gmail.com>
2023-06-04 22:29:55 +0200

    feat: add button

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,34 @@ import './App.css'
 function App() {
   return (
     <>
-      On work ...
+      <div className='display'>
+        <h1> 0 </h1>
+      </div>
+      <div className='button'>
+        <button className='btn reset'> AC </button>
+        <button className='btn operator'> +/- </button>
+        <button className='btn operator'> % </button>
+        <button className='btn operator'> ÷ </button>
+
+        <button className='btn number'> 7 </button>
+        <button className='btn number'> 8 </button>
+        <button className='btn number'> 9 </button>
+        <button className='btn operator'> X </button>
+
+        <button className='btn number'> 4 </button>
+        <button className='btn number'> 5 </button>
+        <button className='btn number'> 6 </button>
+        <button className='btn operator'> - </button>
+
+        <button className='btn number'> 1 </button>
+        <button className='btn number'> 2 </button>
+        <button className='btn number'> 3 </button>
+        <button className='btn operator'> + </button>
+
+        <button className='btn number'> 0 </button>
+        <button className='btn number'> . </button>
+        <button className='btn operator'> = </button>
+      </div>
     </>
   )
 }
