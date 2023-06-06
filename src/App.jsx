9df585456b62b66207149dd15a0cb5c9d13c0799
LeeBingler <binglerlee@gmail.com>
2023-06-06 15:10:20 +0200

    feat: add onClick for number

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
       <Display nbrDisplay={nbrDisplay} />
-      <ButtonInterface />
+      <ButtonInterface setNbrDisplay={setNbrDisplay} setOperator={setOperator} />
     </>
   )
 }
--- src/component/ButtonInterface.jsx
+++ src/component/ButtonInterface.jsx
@@ -1,29 +1,40 @@
 import React from 'react'
 
-function ButtonInterface() {
+function ButtonInterface({ setNbrDisplay, setOperator}) {
+
+  function changeNbr(nbr) {
+    nbr = Number(nbr);
+    console.log(nbr);
+    setNbrDisplay(actualNbr => {
+      if (actualNbr === 0)
+        return nbr;
+      return actualNbr * 10 + nbr;
+    });  
+  }
+
   return (
     <div className='btn-interface'>
-        <button className='btn reset'> AC </button>
-        <button className='btn sign'> +/- </button>
-        <button className='btn percent'> % </button>
+        <button className='btn'> AC </button>
+        <button className='btn'> +/- </button>
+        <button className='btn'> % </button>
         <button className='btn operator'> ÷ </button>
 
-        <button className='btn number'> 7 </button>
-        <button className='btn number'> 8 </button>
-        <button className='btn number'> 9 </button>
+        <button onClick={event => changeNbr(event.target.innerHTML)} className='btn number'> 7 </button>
+        <button onClick={event => changeNbr(event.target.innerHTML)} className='btn number'> 8 </button>
+        <button onClick={event => changeNbr(event.target.innerHTML)} className='btn number'> 9 </button>
         <button className='btn operator'> x </button>
 
-        <button className='btn number'> 4 </button>
-        <button className='btn number'> 5 </button>
-        <button className='btn number'> 6 </button>
+        <button onClick={event => changeNbr(event.target.innerHTML)} className='btn number'> 4 </button>
+        <button onClick={event => changeNbr(event.target.innerHTML)} className='btn number'> 5 </button>
+        <button onClick={event => changeNbr(event.target.innerHTML)} className='btn number'> 6 </button>
         <button className='btn operator'> - </button>
 
-        <button className='btn number'> 1 </button>
-        <button className='btn number'> 2 </button>
-        <button className='btn number'> 3 </button>
+        <button onClick={event => changeNbr(event.target.innerHTML)} className='btn number'> 1 </button>
+        <button onClick={event => changeNbr(event.target.innerHTML)} className='btn number'> 2 </button>
+        <button onClick={event => changeNbr(event.target.innerHTML)} className='btn number'> 3 </button>
         <button className='btn operator'> + </button>
 
-        <button className='btn number zero'> 0 </button>
+        <button onClick={event => changeNbr(event.target.innerHTML)} className='btn number zero'> 0 </button>
         <button className='btn number'> . </button>
         <button className='btn operator'> = </button>
       </div>
