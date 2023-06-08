c20aebde70191bf2d76dca5e7b8bcdff0f473b8a
LeeBingler <binglerlee@gmail.com>
2023-06-08 15:19:13 +0200

    feat: add calculation part

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,18 @@ import ButtonInterface from './component/ButtonInterface';
 function App() {
   const [nbrDisplay, setNbrDisplay] = useState(0);
   const [operator, setOperator] = useState('');
+  const [nbrMemory, setNbrMemory] = useState(0);
 
   return (
     <>
       <Display nbrDisplay={nbrDisplay} />
-      <ButtonInterface setNbrDisplay={setNbrDisplay} setOperator={setOperator} />
+      <ButtonInterface 
+      setNbrDisplay={setNbrDisplay} 
+      setOperator={setOperator} 
+      operator={operator}
+      setNbrMemory={setNbrMemory}
+      nbrMemory={nbrMemory}
+      />
     </>
   )
 }
--- src/component/ButtonInterface.jsx
+++ src/component/ButtonInterface.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 
-function ButtonInterface({ setNbrDisplay, setOperator}) {
+function ButtonInterface({ setNbrDisplay, setOperator, operator, setNbrMemory, nbrMemory}) {
   const [dotClick, setDotClick] = useState(false);
   const [powerForDecimalNbr, setPowerForDecimalNbr] = useState(1);
 
@@ -37,12 +37,49 @@ function ButtonInterface({ setNbrDisplay, setOperator}) {
   }
 
   function changeOperator (operator) {
-    console.log(operator);
-    setOperator(currentOperator => {
-      if (currentOperator != '')
-        return currentOperator;
+    setOperator(() => {
+      setNbrDisplay(currentNbr => {
+        setNbrMemory(currentNbr);
+        return 0;
+      });
       return operator;
     });
+  }
+
+  function makeCalcul() {
+    let nbrCalculate = 0;
+  
+    setNbrDisplay(currentNbr => {
+      switch (operator) {
+        case '+':
+          nbrCalculate = nbrMemory + currentNbr;
+          setOperator('');
+          setNbrMemory(currentNbr);
+          return nbrCalculate;
+
+        case '-':
+          nbrCalculate = nbrMemory - currentNbr;
+          setOperator('');
+          setNbrMemory(currentNbr);
+          return nbrCalculate;
+
+        case 'x':
+          nbrCalculate = nbrMemory * currentNbr;
+          setOperator('');
+          setNbrMemory(currentNbr);
+          return nbrCalculate;
+
+        case '÷':
+          nbrCalculate = nbrMemory / currentNbr;
+          setOperator('');
+          setNbrMemory(currentNbr);
+          return nbrCalculate;
+          
+        default:
+          console.log(operator);
+          return 'Error !';
+      }
+    })
   }
 
   return (
@@ -50,26 +87,26 @@ function ButtonInterface({ setNbrDisplay, setOperator}) {
         <button onClick={() => resetClick()} className='btn'> AC </button>
         <button onClick={() => changeSign()} className='btn'> +/- </button>
         <button onClick={() => percentageClick()} className='btn'> % </button>
-        <button onClick={event => changeOperator(event.target.innerHTML)} className='btn operator'> ÷ </button>
+        <button onClick={event => changeOperator(event.target.innerText)} className='btn operator'> ÷ </button>
 
         <button onClick={event => changeNbr(event.target.innerHTML)} className='btn number'> 7 </button>
         <button onClick={event => changeNbr(event.target.innerHTML)} className='btn number'> 8 </button>
         <button onClick={event => changeNbr(event.target.innerHTML)} className='btn number'> 9 </button>
-        <button onClick={event => changeOperator(event.target.innerHTML)} className='btn operator'> x </button>
+        <button onClick={event => changeOperator(event.target.innerText)} className='btn operator'> x </button>
 
         <button onClick={event => changeNbr(event.target.innerHTML)} className='btn number'> 4 </button>
         <button onClick={event => changeNbr(event.target.innerHTML)} className='btn number'> 5 </button>
         <button onClick={event => changeNbr(event.target.innerHTML)} className='btn number'> 6 </button>
-        <button onClick={event => changeOperator(event.target.innerHTML)} className='btn operator'> - </button>
+        <button onClick={event => changeOperator(event.target.innerText)} className='btn operator'> - </button>
 
         <button onClick={event => changeNbr(event.target.innerHTML)} className='btn number'> 1 </button>
         <button onClick={event => changeNbr(event.target.innerHTML)} className='btn number'> 2 </button>
         <button onClick={event => changeNbr(event.target.innerHTML)} className='btn number'> 3 </button>
-        <button onClick={event => changeOperator(event.target.innerHTML)} className='btn operator'> + </button>
+        <button onClick={event => changeOperator(event.target.innerText)} className='btn operator'> + </button>
 
         <button onClick={event => changeNbr(event.target.innerHTML)} className='btn number zero'> 0 </button>
         <button onClick={() => setDotClick(true)} className='btn number'> . </button>
-        <button className='btn operator'> = </button>
+        <button onClick={() => makeCalcul()} className='btn operator'> = </button>
       </div>
   )
 }
