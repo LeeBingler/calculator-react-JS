ea844bf00dc79acbf87ea93aab5e1180c96c5f7b
LeeBingler <binglerlee@gmail.com>
2023-06-08 14:47:21 +0200

    feat: button opeartor

--- src/component/ButtonInterface.jsx
+++ src/component/ButtonInterface.jsx
@@ -27,6 +27,7 @@ function ButtonInterface({ setNbrDisplay, setOperator}) {
 
   function resetClick() {
     setNbrDisplay(0);
+    setPowerForDecimalNbr(1);
     setDotClick(false);
     setOperator('');
   }
@@ -35,27 +36,36 @@ function ButtonInterface({ setNbrDisplay, setOperator}) {
     setNbrDisplay(actualNbr => actualNbr * -1);
   }
 
+  function changeOperator (operator) {
+    console.log(operator);
+    setOperator(currentOperator => {
+      if (currentOperator != '')
+        return currentOperator;
+      return operator;
+    });
+  }
+
   return (
     <div className='btn-interface'>
         <button onClick={() => resetClick()} className='btn'> AC </button>
         <button onClick={() => changeSign()} className='btn'> +/- </button>
         <button onClick={() => percentageClick()} className='btn'> % </button>
-        <button className='btn operator'> ÷ </button>
+        <button onClick={event => changeOperator(event.target.innerHTML)} className='btn operator'> ÷ </button>
 
         <button onClick={event => changeNbr(event.target.innerHTML)} className='btn number'> 7 </button>
         <button onClick={event => changeNbr(event.target.innerHTML)} className='btn number'> 8 </button>
         <button onClick={event => changeNbr(event.target.innerHTML)} className='btn number'> 9 </button>
-        <button className='btn operator'> x </button>
+        <button onClick={event => changeOperator(event.target.innerHTML)} className='btn operator'> x </button>
 
         <button onClick={event => changeNbr(event.target.innerHTML)} className='btn number'> 4 </button>
         <button onClick={event => changeNbr(event.target.innerHTML)} className='btn number'> 5 </button>
         <button onClick={event => changeNbr(event.target.innerHTML)} className='btn number'> 6 </button>
-        <button className='btn operator'> - </button>
+        <button onClick={event => changeOperator(event.target.innerHTML)} className='btn operator'> - </button>
 
         <button onClick={event => changeNbr(event.target.innerHTML)} className='btn number'> 1 </button>
         <button onClick={event => changeNbr(event.target.innerHTML)} className='btn number'> 2 </button>
         <button onClick={event => changeNbr(event.target.innerHTML)} className='btn number'> 3 </button>
-        <button className='btn operator'> + </button>
+        <button onClick={event => changeOperator(event.target.innerHTML)} className='btn operator'> + </button>
 
         <button onClick={event => changeNbr(event.target.innerHTML)} className='btn number zero'> 0 </button>
         <button onClick={() => setDotClick(true)} className='btn number'> . </button>
